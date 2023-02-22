c610f81ac193155442a43a9a24795c68d8c7aa07
Miran-Mirantee <killerm11805@hotmail.com>
2023-02-23 01:06:04 +0700

    Implement Graph class

---
+++  knightMoves.js
@@ -0,0 +1,14 @@
+class Graph {
+  constructor(data) {
+    this.adjList = new Map();
+  }
+
+  addVertex(v) {
+    this.adjList.set(v, []);
+  }
+
+  addEdge(start, dest) {
+    this.adjList.get(start).push(dest);
+    this.adjList.get(dest).push(start);
+  }
+}
